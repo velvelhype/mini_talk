1f725d6859dfce4a13163f428abd8249fd88598a
velvelhype <velvelhype@gmail.com>
2021-12-28 23:34:08 +0900

    part

--- tsukuba_client.c
+++ tsukuba_client.c
@@ -7,24 +7,20 @@
 void	send_char(int pid, char a)
 {
 	printf("c: send %c\n", a);
-	printf("c: send %d\n", a);
 	int i = 7;
 	while(i >= 0)
 	{
-		printf("%d\n", a >> i & 1);
 		if (a >> i & 1)
 			kill(pid, SIGUSR1);	
 		else
 			kill(pid, SIGUSR2);	
 		usleep(100);
-		// sleep(1);
 		i--;
 	}
 }
 
 void	send_str(char **argv)
 {
-	// send_str by char
 	printf("s: send %s\n", argv[2]);
 	
 	while(*argv[2])
@@ -36,5 +32,7 @@ void	send_str(char **argv)
 
 int main(int argc, char **argv)
 {
+	if(argc != 3)
+		exit(1);
 	send_str(argv);
 }
--- tsukuba_sig_ex.c
+++ tsukuba_sig_ex.c
@@ -8,7 +8,6 @@ int     char_stock = 0;
 
 void    sigint_handler(int signum)
 {
-	// printf("signum %d\n", signum);
 	if(signum == SIGUSR1)
 		char_stock += 1;
 }
@@ -17,11 +16,9 @@ int main()
 {
 	printf("%d\n", getpid());
 	struct sigaction sa_sigint;
-
 	memset(&sa_sigint, 0, sizeof(sa_sigint));
 	sa_sigint.sa_handler = sigint_handler;
 	sa_sigint.sa_flags = SA_RESTART;
-
 	if (sigaction(SIGUSR1, &sa_sigint, NULL) < 0) {
 		perror("sigaction");
 		exit(1);
@@ -34,19 +31,15 @@ int main()
 	while (1) 
 	{
 		pause();
-		// printf("char_stock: %d\n", char_stock);
 		count++;
-		if(count != 8)
+		if (count != 8)
 			char_stock <<= 1;
 		else
 		{
-		    //  printf("sent : %c\n", char_stock);
-			 write(1, &char_stock, 1);
-		     count = 0;
-		     char_stock = 0;
-		//      putchar(char_stock);
+			write(1, &char_stock, 1);
+		    count = 0;
+		    char_stock = 0;
 		}
-		// printf("main: return from pause. sigint_count(%d)\n",
-		//     sigint_count);
+		// printf("cs: %d\n",char_stock);
 	}
 }
